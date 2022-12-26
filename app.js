63af3241d0f103a4abc5046f7391dd65edfd8c8d
Rishabh Dubey <riishabhdubey8@gmail.com>
2022-12-26 09:12:33 +0530

    initial

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const PORT = process.env.port || 3000;
+const app = express();
+app.get("/", function (req, res) {
+  res.send("<h1>Hello</h1>");
+});
+app.listen(PORT, function (req, res) {
+  console.log("Server is running on:" + PORT);
+});
